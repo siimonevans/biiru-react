1101cfa121a3830e5384cf3087712b8ead13295d
Simon Evans <simon.evans@torchbox.com>
2017-02-11 21:40:33 +0000

    Comment some things

--- src/components/BeerList/BeerList.js
+++ src/components/BeerList/BeerList.js
@@ -2,27 +2,32 @@ import React, { Component } from 'react';
 import './BeerList.css';
 
 class BeerList extends Component {
-
   componentDidMount() {
     fetch( 'https://api.punkapi.com/v2/beers' )
       .then( response => response.json() )
       .then( arr => {
+
+        // Map beers
         const beers = arr.map(function(drink) {
-          console.log(drink);
-          const bev = {
+
+          // Grab the props we want
+          const beer = {
             name : drink.name,
             abv : drink.abv,
           }
-          return bev
+
+          return beer
         })
 
         const beerList = document.querySelector( 'main ul' );
 
-        beers.map(function(item){
+        // Add DOM element for each beer and apply props
+        beers.map(function(drink){
+          
           // eslint-disable-next-line
-          beerList.innerHTML += '<li class="beer-list__item"><div class="inner"><div class="beer-name">' + item.name + '</div><div class="beer-abv">' + item.abv + '%' + '</div></div></li>'
+          beerList.innerHTML += '<li class="beer-list__item"><div class="inner"><div class="beer-name">' + drink.name + '</div><div class="beer-abv">' + drink.abv + '%' + '</div></div></li>'
           return null;
-        })
+      })
     })
   }
 
